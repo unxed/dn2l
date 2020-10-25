31035df0d9d3803471dd40063f32252b7e1160a7
unxed <unxed@mail.ru>
2020-10-26 02:34:08 +0300

    no need to delete .INI file in build/run script any more

--- dn_.sh
+++ dn_.sh
@@ -9,7 +9,8 @@ fi
 read -n1 -r -p "Press any key (try Ctrl-C to quit DN)..."
 ../vp/pe2elf dn.exe 
 chmod +x dn
-rm DN.INI
+#was needed for some previous builds to run
+#rm DN.INI
 ./dn > dn.123
 cat dn.123
 read -n1 -r -p "Press any key..."
