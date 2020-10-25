738abbd3c7f1a1aa4bba7ea6c4ceb470280503f5
unxed <unxed@mail.ru>
2020-10-25 19:25:35 +0300

    incremental builds

--- dn_.sh
+++ dn_.sh
@@ -1,9 +1,11 @@
 #/bin/bash
-../vp/vpc -B -T -\$S- -\$I- -\$Zd+ -\$Asm+ -\$D+ -\$LocInfo+ version.pas
-../vp/pe2elf version.exe
-chmod +x version
-./version version.inc linux 0
-../vp/vpc -B -T -\$S- -\$I- -\$Zd+ -\$Asm+ -\$D+ -\$LocInfo+ dn.pas 
+if [ ! -f version.inc ]; then
+    ../vp/vpc -M -T -\$S- -\$I- -\$Zd+ -\$Asm+ -\$D+ -\$LocInfo+ version.pas
+    ../vp/pe2elf version.exe
+    chmod +x version
+    ./version version.inc linux 0
+fi
+../vp/vpc -M -T -\$S- -\$I- -\$Zd+ -\$Asm+ -\$D+ -\$LocInfo+ dn.pas 
 read -n1 -r -p "Press any key (try Ctrl-C to quit DN)..."
 ../vp/pe2elf dn.exe 
 chmod +x dn
