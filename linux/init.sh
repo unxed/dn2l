c5642cd150cbb94b9049e019d36d3f3d2cc2a90f
Ivan Sorokin <unxed@mail.ru>
2022-02-03 00:45:28 +0300

    fix build on ubuntu 20.04

--- linux/init.sh
+++ linux/init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-if ! command -v 7z &> /dev/null
+if ! command -v unrar &> /dev/null
 then
-    echo "7z not found. trying to install via apt"
-    echo "install p7zip-full and p7zip-rar manually if you are using non deb-based distro"
-    sudo apt install p7zip-full p7zip-rar
+    echo "unrar not found. trying to install via apt"
+    echo "install unrar manually if you are using non deb-based distro"
+    sudo apt install unrar
+fi
+if ! command -v unzip &> /dev/null
+then
+    echo "unzip not found. trying to install via apt"
+    echo "install unzip manually if you are using non deb-based distro"
+    sudo apt install unzip
 fi
 if ! command -v rename &> /dev/null
 then
@@ -28,33 +34,34 @@ mkdir vp_full
 (
 cd vp_full
 #wget https://web.archive.org/web/20140919203444/http://old-dos.ru/dl.php?id=2044
-#wget http://old-dos.ru/dl.php?id=2044
+wget http://old-dos.ru/dl.php?id=2044
 #mv dl.php\?id=2044 vp.zip
 cp "../dn2l/vp_dist/Virtual Pascal.ver.2.1.279.English.zip" ./vp.zip
-7z x vp.zip
+unzip vp.zip
 rm -rf vp.zip
 # rtl
 mkdir artlsrc
 cp artlsrc.rar artlsrc/
 (
 cd artlsrc
-7z x artlsrc.rar
+unrar e artlsrc.rar
 rm -rf artlsrc.rar
 rename 'y/A-Z/a-z/' ./*
 cp dos.pas math.pas strings.pas sysutils.pas use32.pas vpsyslnx.pas vpsyslow.pas vputils.pas windos.pas ../../dn2l
 # system.pas system.vps
-(
-cd sys
-rename 'y/A-Z/a-z/' ./*
-cp system.pas system.vps ../../../dn2l
-)
+##(
+##cd sys
+##rename 'y/A-Z/a-z/' ./*
+##cp system.pas system.vps ../../../dn2l
+cp system.pas system.vps ../../dn2l
+##)
 )
 # linux.pas lnxres.pas 
 mkdir lrtlsrc
 cp lrtlsrc.rar lrtlsrc/
 (
 cd lrtlsrc
-7z x lrtlsrc.rar
+unrar e lrtlsrc.rar
 rm -rf lrtlsrc.rar
 rename 'y/A-Z/a-z/' ./*
 cp linux.pas lnxres.pas ../../dn2l
@@ -64,7 +71,7 @@ mkdir lresbin
 cp lresbin.rar lresbin/
 (
 cd lresbin
-7z x lresbin.rar
+unrar e lresbin.rar
 rm -rf lresbin.rar
 rename 'y/A-Z/a-z/' ./*
 cp linux.res sysutils.res ../../dn2l
@@ -74,7 +81,7 @@ mkdir lvpbase
 cp lvpbase.rar lvpbase/
 (
 cd lvpbase
-7z x lvpbase.rar
+unrar e lvpbase.rar
 rm -rf lvpbase.rar
 rename 'y/A-Z/a-z/' ./*
 )
@@ -84,7 +91,7 @@ mkdir aidevph
 cp aidevph.rar aidevph/
 (
 cd aidevph
-7z x aidevph.rar
+unrar e aidevph.rar
 rm -rf aidevph.rar
 rename 'y/A-Z/a-z/' ./*
 )
